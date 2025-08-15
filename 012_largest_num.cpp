ef51e6391ca019796268b73105acea133f7a65ca
Neha Malbari <nmalbari26@gmail.com>
2025-08-16 01:28:20 +0530

    Update 012_largest_num.cpp

--- 012_largest_num.cpp
+++ 012_largest_num.cpp
@@ -1,20 +1,19 @@
-#include <iostream>
+#include <iostream> // for input/output
 using namespace std;
 int main()
 {
-    int num1,num2;
-    cout<<"Enter first number: ";
-    cin>>num1;
-    cout<<"Enter second number: ";
-    cin>>num2;
-    if(num1 > num2)
+    int num1, num2;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    if(num1 > num2) // compare numbers
     {
-        cout<<num1<<" largest";
+        cout << num1 << " largest";
     }
     else
     {
-        cout<<num2<<" largest";
+        cout << num2 << " largest";
     }
     return 0;
-    
 }
