8ced86da855df3f18b5337b303994752a4ab4778
Neha Malbari <nmalbari26@gmail.com>
2025-08-16 01:26:46 +0530

    Update 010_even_odd.cpp

--- 010_even_odd.cpp
+++ 010_even_odd.cpp
@@ -1,18 +1,17 @@
-#include <iostream>
+#include <iostream> // for input/output
 using namespace std;
 int main()
 {
     int number;
-    cout<<"Enter the number: ";
-    cin>>number;
-    if(number % 2 == 0)
+    cout << "Enter the number: ";
+    cin >> number;
+    if(number % 2 == 0) // check if divisible by 2
     {
-        cout<<"Even number\n";
+        cout << "Even number\n";
     }
     else
     {
-        cout<<"Odd number\n";
+        cout << "Odd number\n";
     }
     return 0;
-
 }
