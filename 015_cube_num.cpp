20f5a26c59d5278ed8fd2c8534684f66cc53ca1e
Neha Malbari <nmalbari26@gmail.com>
2025-08-16 01:30:08 +0530

    Update 015_cube_num.cpp

--- 015_cube_num.cpp
+++ 015_cube_num.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
+#include <iostream> // for input/output
 using namespace std;
 int main()
 {
-    int num,cube;
-    cout<<"Enter the number:";
-    cin>>num;
-    cube=num * num * num;
-    cout<<"Cube of "<<num<<" is "<<cube;
+    int num, cube;
+    cout << "Enter the number: ";
+    cin >> num;
+    cube = num * num * num; // calculate cube
+    cout << "Cube of " << num << " is " << cube; // display result
     return 0;
 }
