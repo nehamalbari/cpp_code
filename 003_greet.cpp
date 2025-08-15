21a0e86a76e87f6b6b2e6059fcc471517eacbb31
Neha Malbari <nmalbari26@gmail.com>
2025-08-16 01:20:58 +0530

    Update 003_greet.cpp

--- 003_greet.cpp
+++ 003_greet.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // for input/output
+using namespace std; 
 int main()
 {
     string name;
-    cout<<"Enter your name:";
+    cout<<"Enter your name:"; // prompt and take name input
     cin>>name;
-    cout<<"Namah Shivay "<<name<<endl;
+    cout<<"Namah Shivay "<<name<<endl; // display greeting
     return 0;
-
 }
+
