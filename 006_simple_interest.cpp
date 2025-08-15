094c102bc5e732ccdac446aaca536cb7ae926d38
Neha Malbari <nmalbari26@gmail.com>
2025-08-16 01:23:18 +0530

    Update 006_simple_interest.cpp

--- 006_simple_interest.cpp
+++ 006_simple_interest.cpp
@@ -1,16 +1,15 @@
-#include <iostream>
+#include <iostream> // for input/output
 using namespace std;
 int main()
 {
-    double principal,roi,time,si;
-    cout<<"Enter the amount: ";
-    cin>>principal;
-    cout<<"Enter annual interest rate: ";
-    cin>>roi;
-    cout<<"Enter the time period (in years): ";
-    cin>>time;
-    si=(principal * roi * time) / 100;
-    cout<<"Simple Interest = "<<si<<endl;
+    double principal, roi, time, si;
+    cout << "Enter the amount: ";
+    cin >> principal;
+    cout << "Enter annual interest rate: ";
+    cin >> roi;
+    cout << "Enter the time period (in years): ";
+    cin >> time;
+    si = (principal * roi * time) / 100; // calculate simple interest
+    cout << "Simple Interest = " << si << endl; // display result
     return 0;
-
 }
