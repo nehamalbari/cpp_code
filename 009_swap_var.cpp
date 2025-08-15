f2e5bad3ac8f55bee2f80f41b0e272d863b0824e
Neha Malbari <nmalbari26@gmail.com>
2025-08-16 01:25:59 +0530

    Update 009_swap_var.cpp

--- 009_swap_var.cpp
+++ 009_swap_var.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
+#include <iostream> // for input/output
 using namespace std;
 int main()
 {
-    string first_name,last_name,temp;
-    cout<<"Enter your first name: ";
-    cin>>first_name;
-    cout<<"Enter your last name: ";
-    cin>>last_name;
-    temp=first_name;
-    first_name=last_name;
-    last_name=temp;
-    cout<<"Your firstname is "<<first_name<<"\n";
-    cout<<"Your lastname is "<<last_name<<"\n";
+    string first_name, last_name, temp;
+    cout << "Enter your first name: ";
+    cin >> first_name;
+    cout << "Enter your last name: ";
+    cin >> last_name;
+    temp = first_name; // store first name temporarily
+    first_name = last_name; // swap values
+    last_name = temp;
+    cout << "Your firstname is " << first_name << "\n"; // display swapped first name
+    cout << "Your lastname is " << last_name << "\n"; // display swapped last name
     return 0;
-
 }
+
