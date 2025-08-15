1ad06d5580e216a108d5f8190e9d4d297f774229
Neha Malbari <nmalbari26@gmail.com>
2025-08-16 01:34:27 +0530

    Update 004_area_rect.cpp

--- 004_area_rect.cpp
+++ 004_area_rect.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 int main()
 {
-    int length=10, width=20, area;
+    double length, width,area;
+    cout<<"Enter the length of rectangle"; //prompt user
+    cin>>length;   //read length
+    cout<<"Enter the width of rectangle";   //prompt user
+    cin>>width;     // read width
     area = length * width; // calculate area
     cout << area << endl; // display result
     return 0;
