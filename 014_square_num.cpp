126be22269dcee1a7fd21ad20f56f3143702a98c
Neha Malbari <nmalbari26@gmail.com>
2025-08-16 01:29:33 +0530

    Update 014_square_num.cpp

--- 014_square_num.cpp
+++ 014_square_num.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
+#include <iostream> // for input/output
 using namespace std;
 int main()
 {
-    int num,square;
-    cout<<"Enter the number:";
-    cin>>num;
-    square=num * num;
-    cout<<"Square of "<<num<<" is "<<square;
+    int num, square;
+    cout << "Enter the number: ";
+    cin >> num;
+    square = num * num; // calculate square
+    cout << "Square of " << num << " is " << square; // display result
     return 0;
 }
