b6a37ba010bb3bca69e1707e60fedfa5fad058e0
Neha Malbari <nmalbari26@gmail.com>
2025-08-16 01:27:35 +0530

    Update 011_average.cpp

--- 011_average.cpp
+++ 011_average.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
+#include <iostream> // for input/output
 using namespace std;
 int main()
 {
-    float num1,num2,num3;
+    float num1, num2, num3;
     double avg;
-    cout<<"Enter first number: ";
-    cin>>num1;
-    cout<<"Enter second number: ";
-    cin>>num2;
-    cout<<"Enter third number: ";
-    cin>>num3;
-    avg=(num1 + num2 + num3)/3.0;
-    cout<<"Average = "<<avg;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    cout << "Enter third number: ";
+    cin >> num3;
+    avg = (num1 + num2 + num3) / 3.0; // calculate average
+    cout << "Average = " << avg; // display result
     return 0;
-    
 }
