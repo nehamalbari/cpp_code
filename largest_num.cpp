05a7a53fefab08813389249a3e8051e3c1a681f6
Neha <nmalbari26@gmail.com>
2025-08-16 00:40:10 +0530

    Largest number

---
+++ largest_num.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"Enter first number: ";
+    cin>>num1;
+    cout<<"Enter second number: ";
+    cin>>num2;
+    if(num1 > num2)
+    {
+        cout<<num1<<" largest";
+    }
+    else
+    {
+        cout<<num2<<" largest";
+    }
+    return 0;
+    
+}
