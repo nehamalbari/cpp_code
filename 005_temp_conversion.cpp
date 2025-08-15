0423faf83bff8f304e4c96d5f52585e475cd6de8
Neha Malbari <nmalbari26@gmail.com>
2025-08-16 01:22:38 +0530

    Update 005_temp_conversion.cpp

--- 005_temp_conversion.cpp
+++ 005_temp_conversion.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
+#include <iostream> // for input/output
 using namespace std;
 int main()
 {
-    double celsius,fahrenheit;
-    cout<<"Enter the temperature in celsius: ";
-    cin>>celsius;
-    fahrenheit=(9.0 / 5.0 * celsius)+32;
-    cout<<"The temperature in Fahrenheit is "<<fahrenheit<<endl;
+    double celsius, fahrenheit;
+    cout << "Enter the temperature in celsius: "; // take input
+    cin >> celsius;
+    fahrenheit = (9.0 / 5.0 * celsius) + 32; // convert to Fahrenheit
+    cout << "The temperature in Fahrenheit is " << fahrenheit << endl; // display result
     return 0;
 }
