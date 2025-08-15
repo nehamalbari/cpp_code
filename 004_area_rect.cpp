7eda206e9ec18e840d334a1115a4dc07e6cb04ba
Neha Malbari <nmalbari26@gmail.com>
2025-08-16 01:21:49 +0530

    Update 004_area_rect.cpp

--- 004_area_rect.cpp
+++ 004_area_rect.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+#include <iostream> // for input/output
 using namespace std;
 int main()
 {
-    int length=10,width=20,area;
-    area=length * width;
-    cout<<area<<endl;
+    int length=10, width=20, area;
+    area = length * width; // calculate area
+    cout << area << endl; // display result
     return 0;
 }
