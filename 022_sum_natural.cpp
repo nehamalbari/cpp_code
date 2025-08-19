9a06770602306e7c67d1afa78bad9e6f0f3f4294
Neha <nmalbari26@gmail.com>
2025-08-20 00:23:03 +0530

    Adding first n natural numbers

---
+++ 022_sum_natural.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,sum=0;//sum will store the cumulative sum of natural numbers upto a limit
+    cout<<"Enter the limit:";
+    cin>>n;
+    for(int i=1;i<=n;i++)//loop for printing first 10 natural numbers
+    {
+         sum=sum+i;//add the current natural number i to the sum
+    }
+    cout<<"The sum of "<<n<<" natural numbers are "<<sum;
+    return 0;
+}
