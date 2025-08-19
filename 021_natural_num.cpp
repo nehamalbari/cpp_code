59ca008d2d9556b25e77c2a89980d0fc46be3fd8
Neha <nmalbari26@gmail.com>
2025-08-20 00:12:55 +0530

    Printing first 10 natural numbers

---
+++ 021_natural_num.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=10;i++)//loop for printing first 10 natural numbers
+    {
+        cout<<i<<"\n";
+    }
+    return 0;
+}
