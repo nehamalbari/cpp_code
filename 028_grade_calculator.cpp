06f6cb79f831476b8469f320f7a9cad3a418fa61
Neha <nmalbari26@gmail.com>
2025-08-20 21:09:29 +0530

    To calculate the grades of the student

--- 028_grade_calculator.cpp
+++ 028_grade_calculator.cpp
@@ -5,30 +5,37 @@ int main()
     int marks;
     cout<<"Enter your marks:";
     cin>>marks;
+    // If marks are 90 or greater, assign an 'O grade'.
     if(marks>=90)
     {
         cout<<"O grade";
     }
+    //Otherwise If marks are 80 or greater, assign an 'A grade'.
     else if(marks>=80)
     {
         cout<<"A grade";
     }
+    //Otherwise If marks are 70 or greater, assign an 'B grade'.
     else if(marks>=70)
     {
         cout<<"B grade";
     }
+    //Otherwise If marks are 60 or greater, assign an 'C grade'.
     else if(marks>=60)
     {
         cout<<"C grade";
     }
+    //Otherwise If marks are 50 or greater, assign an 'D grade'.
     else if(marks>=50)
     {
         cout<<"D grade";
     }
+    //Otherwise If marks are 40 or greater, assign an 'E grade'.
     else if(marks>=40)
     {
         cout<<"E grade";
     }
+     // If none of the above conditions are met, the marks are less than 40, so the student 'Fails'.
     else 
     {
         cout<<"Fail";
