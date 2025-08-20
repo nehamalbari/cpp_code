88b9b333fe797130d2c4f8996f5bbecbd5a44bcb
Neha <nmalbari26@gmail.com>
2025-08-20 20:59:53 +0530

    Check whether the year is leap or not

--- 026_check_leapyear.cpp
+++ 026_check_leapyear.cpp
@@ -5,13 +5,14 @@ int main()
     int year;
     cout<<"Enter the year: ";
     cin>>year;
-    if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) )
+    // The condition checks if the year is divisible by 400, OR if it's divisible by 4 AND NOT divisible by 100.
+    if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
     {
-        cout<<year<<" is a leap year ";
+        cout<<year<<" is a leap year ";//executed when the condition is true
     }
     else
     {
-        cout<<year<<" is not a leap year ";
+        cout<<year<<" is not a leap year ";//executed when the condition is false
     }
     return 0;
 }
