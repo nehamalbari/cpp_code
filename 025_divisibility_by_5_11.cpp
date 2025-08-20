90a4578b8254be4479bb7a050730d654746978af
Neha <nmalbari26@gmail.com>
2025-08-20 19:29:51 +0530

    Check if a num is divisible by 5 and 11

--- 025_divisibility_by_5_11.cpp
+++ 025_divisibility_by_5_11.cpp
@@ -10,21 +10,10 @@ int main()
     {
         cout<<num<<" is divisible by 5 and 11 ";
     }
-    // Checks if the number is divisible by 5 
-    else if(num % 5 == 0)
-    {
-        cout<<num<<" is divisible by 5";
-    }
-    // Checks if the number is divisible by 11.
-
-    else if(num % 11 == 0)
-    {
-        cout<<"Divisible by 11";
-    }
-    // Executes if none of the above conditions are true.
+    //executes when the above condition becomes false
     else
     {
-        cout<<" is not divisible by either 5 or 11 ";
+        cout<<num<<" is not divisible by 5 and 11 ";
     }
     return 0;
 
