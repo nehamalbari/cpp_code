0fa5a7227be5a97de972deb41545c42ae92d2304
Neha <nmalbari26@gmail.com>
2025-08-20 21:22:45 +0530

    CHecks whether a character is an alphabet

---
+++ 029_alphabet_checker.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cout<<"Enter the character:";
+    cin>>c;
+     // This 'if' statement checks if the entered character is an alphabet.
+    if((c >= 'a' && c<= 'z') || (c >= 'A' && c <= 'Z'))
+    {
+        cout<<c<<" is an alphabet";//executes when condition is true
+    }
+    else
+    {
+        cout<<c<<" is not an alphabet";//executes when condition is false
+    }
+    return 0;
+
+}
