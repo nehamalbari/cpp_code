9eb6ac0f0b4832f71e1405c096adce515d9cce51
Neha <nmalbari26@gmail.com>
2025-08-21 23:41:57 +0530

    print numbers from n to 1

---
+++ 040_print_num_n_to_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long limit;
+    cout<<"Enter the limit: ";
+    cin>>limit;
+    for(int i=limit;i>=1;i--)//loop to iterate from 1 to limit
+    {
+        cout<<i<<"\n";
+    }
+    return 0;
+
+}
