d4d131a0a6174d477cd584e3e3a834cd2d6ef011
Neha <nmalbari26@gmail.com>
2025-08-21 14:54:49 +0530

    Day of the week based on number

---
+++ 031_day_of_week.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int day;
+    cout<<"Enter a number (1-7): ";
+    cin>>day;
+    switch (day)//switch-statement is used to perform the comparisons
+    {
+        //if day = 1 , this case is executed
+    case 1:
+        cout<<"Day is Monday";
+        break;
+        //if day = 1 , this case is executed
+    case 2:           
+        cout<<"Day is Tuesday";
+        break;
+        //if day = 1 , this case is executed
+    case 3:               
+        cout<<"Day is Wednesday";
+        break;
+        //if day = 1 , this case is executed
+    case 4:
+        cout<<"Day is Thursday";
+        break;
+        //if day = 1 , this case is executed.
+    case 5:
+        cout<<"Day is Friday";
+        break;
+        //if day = 1 , this case is executed
+    case 6:
+        cout<<"Day is Saturday";
+        break;
+        //if day = 1 , this case is executed
+    case 7:
+        cout<<"Day is Sunday";
+        break;
+        // The 'default' case is executed if none of the 'case' labels match the value of 'day'.
+
+    
+    default:
+        cout<<"Invalid Choice";
+        break;
+    }
+    return 0;
+}
